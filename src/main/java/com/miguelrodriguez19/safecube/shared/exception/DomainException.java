f1571d825a95d6f9a7549948cfbcee2217d9dddd
Miguel Rodriguez <rodriguezherreromiguel@gmail.com>
2026-01-14 09:28:47 +0100

    chore(shared): introduce base DomainException and Void result type

---
+++ src/main/java/com/miguelrodriguez19/safecube/shared/exception/DomainException.java
@@ -0,0 +1,8 @@
+package com.miguelrodriguez19.safecube.shared.exception;
+
+public class DomainException extends RuntimeException {
+
+  public DomainException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/com/miguelrodriguez19/safecube/shared/result/Void.java
@@ -0,0 +1,8 @@
+package com.miguelrodriguez19.safecube.shared.result;
+
+public final class Void {
+
+  public static final Void INSTANCE = new Void();
+
+  private Void() {}
+}
