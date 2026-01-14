135b8faf7aefa515b7d5d72624626b0b4f0f459e
Miguel Rodriguez <rodriguezherreromiguel@gmail.com>
2026-01-14 09:30:30 +0100

    feat(user-app): define UserError application error model

---
+++ src/main/java/com/miguelrodriguez19/safecube/user/application/error/UserError.java
@@ -0,0 +1,21 @@
+package com.miguelrodriguez19.safecube.user.application.error;
+
+/**
+ * UserError
+ *
+ * <p>Base type for all expected user-profile-related application errors.
+ */
+public sealed interface UserError
+    permits UserError.UserProfileAlreadyExists,
+        UserError.AccountNotFound,
+        UserError.UserProfileNotFound {
+
+  /** Returned when attempting to create a user profile that already exists. */
+  record UserProfileAlreadyExists() implements UserError {}
+
+  /** Returned when attempting to create a profile for a non-existing account. */
+  record AccountNotFound() implements UserError {}
+
+  /** Returned when attempting to find an unexisting profile */
+  record UserProfileNotFound() implements UserError {}
+}
