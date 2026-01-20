81f52265e8416ebf449aef2034b15677929f1c0b
Miguel Rodriguez <rodriguezherreromiguel@gmail.com>
2026-01-20 01:40:13 +0100

    feat(vault-keys): add domain exceptions

---
+++ src/main/java/com/miguelrodriguez19/safecube/vault/domain/exception/InvalidVaultKeyMaterialException.java
@@ -0,0 +1,11 @@
+package com.miguelrodriguez19.safecube.vault.domain.exception;
+
+
+import com.miguelrodriguez19.safecube.shared.exception.DomainException;
+
+public class InvalidVaultKeyMaterialException extends DomainException {
+
+  public InvalidVaultKeyMaterialException(final String message) {
+    super(message);
+  }
+}
